a5e23c204e06fe7623f26fc1a97a0f5f83849b7d
Thomas Rayden <thomasmwrayden@gmail.com>
2018-10-08 14:45:32 +0800

    fixed and improved Router class

--- lib/Router.ts
+++ lib/Router.ts
@@ -7,32 +7,45 @@ export enum RequestType {
   PATCH
 }
 
+export interface IPreRoute {
+  path: string;
+  steps: Array<(req: any, res: any, next: any) => Promise<any>>;
+}
+
 export interface IRoute {
   type: RequestType;
   path: string;
   steps: Array<(req: any, res: any, next: any) => Promise<any>>;
 }
 
-export interface IRouteGet extends IRoute {
-  type: RequestType.GET;
-}
-
-export interface IRoutePatch extends IRoute {
-  type: RequestType.PATCH;
-}
-
-export interface IRoutePost extends IRoute {
-  type: RequestType.POST;
-}
-
 export class Router {
+  public static createRoutes(type: RequestType, pre: IPreRoute[]): IRoute[] {
+    return pre.map(
+      (route): IRoute => {
+        return {
+          ...route,
+          type
+        };
+      }
+    );
+  }
+
+  public static mergeRoutes(...routes: IRoute[][]): IRoute[] {
+    return [].concat.apply([], routes);
+  }
+
   private router: any;
 
-  constructor(get: IRouteGet[], patch?: IRoutePatch[], post?: IRoutePost[]);
+  constructor(...routes: IRoute[][]);
   constructor(routes: IRoute[]) {
+    routes =
+      arguments.length === 1
+        ? routes
+        : Router.mergeRoutes(...Array.from(arguments));
+
     this.router = express.Router();
 
-    routes.forEach(route => {
+    routes.forEach((route: IRoute) => {
       switch (route.type) {
         case RequestType.GET:
           this.router.get(route.path, steps(...route.steps));
