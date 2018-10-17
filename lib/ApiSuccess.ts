66315f5c7e6cb1cb14f844c5324796b2589e229c
Thomas Rayden <thomasmwrayden@gmail.com>
2018-10-17 18:10:17 +0800

    make data optional for respond static

--- lib/ApiSuccess.ts
+++ lib/ApiSuccess.ts
@@ -2,7 +2,7 @@ import * as express from 'express';
 import * as HTTPStatus from 'http-status';
 
 export class ApiSuccess {
-  public static respond(res: express.Response, data: any): void {
+  public static respond(res: express.Response, data?: any): void {
     res.status(HTTPStatus.OK).json({
       status: 'success',
       data
