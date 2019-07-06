57266ec434a64347aaf431f02341eb8e827d79ab
Thomas Rayden <thomasmwrayden@gmail.com>
2019-07-06 17:32:23 +0800

    Added more tests

--- test/ApiError.test.ts
+++ test/ApiError.test.ts
@@ -1,17 +1,38 @@
 import { ApiUnauthorized } from '../lib/ApiUnauthorized';
+import { handleError } from '../lib/Error';
+import { Request, Response, NextFunction } from 'express';
 
-const mockRequest = () => {
-  return {};
+const mockError = (): Error => {
+  return new Error('This is a test error.');
 };
 
-const mockResponse = () => {
+const mockRequest = (): Request => {
+  return {} as Request;
+};
+
+const mockResponse = (): Response => {
   const res: any = {};
   res.status = jest.fn().mockReturnValue(res);
   res.json = jest.fn().mockReturnValue(res);
   return res;
 };
 
-test('Throw Unauthorized w/ message', () => {
+const mockNext = (): NextFunction => {
+  return () => {};
+};
+
+function throwError() {
+  throw mockError();
+}
+
+test('Handle thrown error', () => {
+  const req = mockRequest();
+  const res = mockResponse();
+  const next = mockNext();
+  handleError(req, res, next)(mockError());
+});
+
+test('Unauthorized w/ message', () => {
   const res = mockResponse();
   const message = 'This is a test message.';
   const instance = new ApiUnauthorized(res, message);
